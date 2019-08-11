c8e60b475adfe7ba92b34ed68ca994d693055551
OksiG <okcana693@mail.ru>
2019-08-11 12:14:34 +0300

    task OOP 2.2.2

---
+++ 2.2-OOP/2.2.2/formActions/addUser.php
@@ -0,0 +1,20 @@
+<?php
+/*Подключение необходимых файлов*/
+include ('autoload.php');
+include ('SystemConfig.php');
+
+/*Создание объекта*/
+$newUser = new User;
+
+/*Передача значений свойств из формы в объект*/
+$newUser->name = $_POST['name'];
+$newUser->email = $_POST['email'];
+$newUser->password = $_POST['password'];
+$newUser->rate = $_POST['rate'];
+
+/*Сохранение*/
+$newUser->commit();
+
+/*Далее перенаправление на страницу, с которой производилась отправка формы:*/
+header('HTTP/1.1 200 OK');
+header('Location: http://' . $_SERVER['HTTP_HOST'] . '/2.2-OOP/2.2.2');
--- 2.2-OOP/2.2.2/index.php
+++ 2.2-OOP/2.2.2/index.php
@@ -2,3 +2,35 @@
 
 include ('autoload.php');
 include ('SystemConfig.php');
+
+$test = new Users;
+$usersList = $test->newQuery()->getObjs();
+print_r($usersList);
+?>
+
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Список пользователей</title>
+    <style type="text/css">
+        input {
+            margin-top: 10px;
+        }
+        button {
+            margin-top: 10px;
+        }
+    </style>
+</head>
+<body>
+
+<form action="./formActions/addUser.php" method="post">
+    <input type="text" name="name" placeholder="Имя" required>
+    <input type="text" name="password" placeholder="Пароль" required>
+    <input type="text" name="email" placeholder="Электронная почта" required>
+    <input type="text" name="rate" placeholder="Рейтинг" required>
+    <button type="submit">Добавить пользователя</button>
+</form>
+</body>
+</html>
+
