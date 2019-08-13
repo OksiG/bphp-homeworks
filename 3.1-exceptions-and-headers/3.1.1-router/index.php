a05b57f7932bb07767ac2710b20bf8071d91e5a1
OksiG <okcana693@mail.ru>
2019-08-14 00:17:46 +0300

    task 3.1.1. router

--- 3.1-exceptions-and-headers/3.1.1-router/index.php
+++ 3.1-exceptions-and-headers/3.1.1-router/index.php
@@ -8,8 +8,34 @@ $availableLinks = include './availableLinks.php';
 
 require './router.php';
 
+class BadRequest extends Exception {}
+class NotFound extends Exception {}
+
 function check($availableLinks) {
-    if (!isset($_GET['page'])
+    if (!isset($_GET['page']) {
+        throw new BadRequest();
+    }
+
+    $try = new Router($availableLinks);
+    $tryResult = $try->isAvailablePage($_GET['page']);
+
+    if (!$tryResult) {
+        throw new NotFound();
+    }
+
+    return '<p>Вы находитесь на странице <b>' . $_GET['page'] . '</b></p><br>';
 }
+    try {
+        echo check($availableLinks);
+    }
+    catch (BadRequest $e) {
+        echo $e->getMessage() . '<br/>';
+        header('Location: error.php', true, 400);
+    }
+    catch (NotFound $e) {
+        echo $e->getMessage() . '<br/>';
+        header('Location: 404.php', true, 404);
+    }
+
 
 
